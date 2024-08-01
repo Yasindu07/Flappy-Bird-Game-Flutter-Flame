7e9eb6ca032f7a0b10ce58dc9b407bc3983d25bf
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-08-01 17:02:44 +0530

    Game background Design

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,3 +1,13 @@
 import 'package:flame/game.dart';
+import 'package:flappy_bird/components/background.dart';
+import 'package:flappy_bird/components/ground.dart';
 
-class FlappyBirdGame extends FlameGame {}
+class FlappyBirdGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    addAll([
+      Background(),
+      Ground(),
+    ]);
+  }
+}
